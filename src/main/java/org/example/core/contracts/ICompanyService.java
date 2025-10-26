e121c100821efee4072d30ebdcb6c36b674dfe54
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2025-10-27 00:30:08 +0200

    Add Company Dto Definitions

--- src/main/java/org/example/core/contracts/ICompanyService.java
+++ src/main/java/org/example/core/contracts/ICompanyService.java
@@ -1,5 +1,10 @@
 package org.example.core.contracts;
 
-public interface ICompanyService {
+import org.example.dto.CreateCompanyDto;
+import org.example.dto.UpdateCompanyDto;
 
+public interface ICompanyService {
+    void createCompany(CreateCompanyDto dto);
+    void updateCompany(UpdateCompanyDto dto);
+    void deleteCompany(long id);
 }
