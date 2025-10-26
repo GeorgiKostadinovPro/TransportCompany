e762712852fb79e7014d21557eb6c7f5982074fe
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2025-10-26 12:02:32 +0200

    Add SessionFactory Utility

--- src/main/java/org/example/configuration/SessionFactoryUtil.java
+++ src/main/java/org/example/configuration/SessionFactoryUtil.java
@@ -1,5 +1,24 @@
 package org.example.configuration;
 
-public class SessionFactoryUtil {
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
+public class SessionFactoryUtil {
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            Configuration configuration = new Configuration();
+            // Declare DB Sets
+            ServiceRegistry serviceRegistry
+                    = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        }
+
+        return sessionFactory;
+    }
 }
